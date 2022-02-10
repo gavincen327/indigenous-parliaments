f054056ee9fe5169f48dcac67aa4fc88a95a8f07
Chuang <98187347+gavincen327@users.noreply.github.com>
2022-02-10 18:21:51 -0500

    Add testing files (pdf to csv)

---
+++ Yukon/test_pdf2csv.py
@@ -0,0 +1,19 @@
+from process_yukon_hansards import process_converted_pdfs
+import process_pdfs as procpdf
+from indig_parl_utils import download_mht, send_text_to_file
+from indig_parl_re import text_rem_patterns, text_extract_pattern
+import pandas as pd
+from get_yukon_hansards import process_pdf
+import csv
+import sys
+import requests
+import pandas as pd
+import textract
+import datetime
+import PyPDF2
+from tika import parser
+
+pdf_path = "test_April 1, 2008.pdf"
+str_date = '2008-04-01'
+file_prefix = str_date
+process_converted_pdfs(pdf_path, str_date, file_prefix)
